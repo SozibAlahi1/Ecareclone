75d567c26998f0cc14376a5c4c192939c158b277
SozibAlahi1 <105582872+SozibAlahi1@users.noreply.github.com>
2023-01-29 22:10:29 +0600

    updateed

--- src/component/pages/Gallery.jsx
+++ src/component/pages/Gallery.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Header from '../header'
+import Header from '../header';
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import Footer from '../Footer';
 function Gallery() {
   return (
     <>
     <Header />
-       <div className="container mt-5 pt-5">
-        <div className="justify-content-center row">
+       <div className="container mt-5 pt-5 mb-5 pb-5">
+        <div className="justify-content-center row mb-5">
             <div className="col-lg-7">
                 <div className="text-center mt-5">
                     <h3 className='fonts fw-bold'>৪৩ তম জাতীয় বিজ্ঞান ও প্রযুক্তি সপ্তাহ ২০২১-বাঘা উপজেলা</h3>
@@ -96,7 +98,29 @@ function Gallery() {
                 </div>
             </div>
             </div>
+            <div className="footertopsection mt-5">
+                <div className="row">
+                    <div className="col-lg-3">
+                    <Player
+                        autoplay
+                        loop
+                        src="https://assets6.lottiefiles.com/packages/lf20_HF41OKnIK9.json"
+                        style={{ height: '400', width: '400' }}
+                        >
+                        <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
+                        </Player>
+                    </div>
+                    <div className="col-lg-3">
+                        <h2 className='ml-3 fonts mt-5 fw-bold'>আমাদের ফেসবুক কমিউনিটিতে যুক্ত হন</h2>
+                    </div>
+                    <div className="col-lg-3">
+                    <center><a href=""><button className='mt-5 mb-5 btn-subscribe'>Join Now</button></a></center>
+                    </div>
+                </div>
+            </div>
        </div>
+
+<Footer />
     </>
   )
 }
